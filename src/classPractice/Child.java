550ff84286bbd008d2d2e4e7fa8b6375955a13fe
변찬우 <chan00106@naver.com>
2021-07-10 22:50:50 +0900

    Method, Field, Constructor

---
+++ src/classPractice/Child.java
@@ -0,0 +1,12 @@
+package classPractice;
+
+public class Child {
+
+	public static void main(String[] args) {
+		Parent a = new Parent("Teemo", 51);
+		
+		a.printName();
+		a.printAge();
+	}
+
+}
---
+++ src/classPractice/Parent.java
@@ -0,0 +1,22 @@
+package classPractice;
+
+public class Parent {
+	//Field
+	String name;
+	int age;
+	
+	//Constructor
+	public Parent(String name, int age){
+		super();
+		this.name = name;
+		this.age = age;
+	}
+	
+	//Method
+	public void printName(){
+		System.out.println(this.name);
+	}
+	public void printAge() {
+		System.out.println(this.age);
+	}
+}
