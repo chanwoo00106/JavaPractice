d08f58be741c4f1e9870dc8e2f228ef5193ac150
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-08-26 11:31:24 +0900

    입출력

---
+++ src/chap02/Calculator.java
@@ -0,0 +1,19 @@
+package chap02;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double num1 = sc.nextDouble();
+        double num2 = sc.nextDouble();
+
+        System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+        System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+        System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
+        System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
+        System.out.println(num1 + " % " + num2 + " = " + (num1 % num2));
+
+    }
+}
