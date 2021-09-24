7da83222bc05ef258e7481fb713d52f0f9b40695
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-09-24 10:10:25 +0900

    상속 배우기

---
+++ src/chap06/Animal.java
@@ -0,0 +1,13 @@
+package chap06;
+
+public class Animal {
+    int mouth;
+
+    void sleep() {
+        System.out.println("잠을 잔다");
+    }
+
+    void eat() {
+        System.out.println(mouth + "개의 입으로 먹다");
+    }
+}
---
+++ src/chap06/Eagle.java
@@ -0,0 +1,9 @@
+package chap06;
+
+public class Eagle extends Animal {
+    int wing;
+
+    void fly() {
+        System.out.println(wing + "개의 날개로 난다");
+    }
+}
