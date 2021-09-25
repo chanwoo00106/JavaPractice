3868d8299e7c7b2704c50283942feb6bafccf263
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-09-25 23:44:53 +0900

    상속 6번 문제

---
+++ src/chap06/Vehicle.java
@@ -0,0 +1,15 @@
+package chap06;
+
+public class Vehicle {
+    String color;
+    int speed;
+
+    public Vehicle(String color, int speed) {
+        this.color = color;
+        this.speed = speed;
+    }
+
+    void show() {
+        System.out.println("color : " + color + ", speed : " + speed);
+    }
+}
